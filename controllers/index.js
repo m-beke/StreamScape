9356671c6d45c5487abfc7e81630a7298da0a564
Robert <robojr181@yahoo.com>
2024-02-08 12:41:17 -0500

    commented out the month,day,year code,implemted time stamps

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 
 // Import all of the routes from controllers here
 
+
+
 // Connect the routes to the router here
 
 module.exports = router;
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,16 @@
-module.exports = {
+
   // function to format a timestamp, accepts the timestamp and an options object as optional parameters
-  formatDate: (date) => {
-    // Format date as MM/DD/YYYY
-    return date.toLocaleDateString();
-  },
-};
+//   formatDate: (date) => {const options = {
+//     locale: 'en-US',
+//     timeZone: 'America/New_York'
+//   };
+  
+//     // Format date as MM/DD/YYYY
+//     function formatDate(date){
+//       return date.toLocaleDateString('en-US');
+//     }
+//     const today = new Date();
+// const formattedDate = formatDate(today);
+// console.log(formattedDate); //feb 8th 2024
+//   },
+// module.exports = {};
